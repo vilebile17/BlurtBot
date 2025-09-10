28cfdebf35374a1d4103b47ba3ead0f7b5c71cee
vilebile_17 <ibrahimsomauroo@proton.me>
2025-09-10 18:41:43 +0100

    Got the bot linked up with discord now, it can say pong! if you say ping

--- gemini.py
+++ gemini.py
@@ -15,7 +15,7 @@ def get_message(previous_messages):
     You are a discord bot which tries to predict the next message based on the last few,
     You will recieve a list of past messages (last item is most recent and first is the oldest) and with that you must align to the tone, abreviations (or lack of them), message length and context to try and be as human as possible.
     I hope you are up for the challenge.
-    All that I need you to return is the message you will send; no name of sender, no date, just simply the message.
+    All that I need you to return is the message you will send; ** NO NAME OF SENDER **, no date, just simply the message.
     """
 
     response = client.models.generate_content(
--- main.py
+++ main.py
@@ -8,15 +8,53 @@ from gemini import get_message
 
 def main():
     message = get_message([
-        "Ibrahim: guys can you bring",
-        "Ibrahim: Some spare clothes and",
-        "Ibrahim: Some walking shoes",
-        "Ibrahim: Thank you",
-        "Yahya: Do walking sandles count as walking shoes?",
-        "Ibrahim: yh ig",
-        "Ali: Ibrahim, what time are u going to pick us up?"
+"""Yakha — 23/07/2025, 03:03
+I shall ask my birthers if I may participate, if you would allow my presence that is""",
+'''XSET-IGL
+[GOAT]
+ — 23/07/2025, 03:03
+🤓''',
+"""peasandbeans34
+[FC]
+ — 23/07/2025, 10:00
+I shall also ask my birther if I may participate, if you would allow my presence that is 
+""",
+"""Ibrahim — 23/07/2025, 11:29
+of course you can all come but i need to 
+remind you i've got 6 seats in my car""",
+"""peasandbeans34
+[FC]
+ — 23/07/2025, 13:10
+we can probably get *escorted* by our birther"""
     ])
-    print(message.text)
 
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f"Logged in as {bot.user}")
+
+    @bot.command()
+    async def hello(ctx):
+        await ctx.send("Hello! Blurt Bot is here and ready to cause some carnage!")
+
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return  
+        print(f"{message.author}: {message.content}")
+        if message.content.lower() == "ping":
+            await message.channel.send("pong!")
+        await bot.process_commands(message)
+ 
+    load_dotenv()
+    discord_key = os.environ.get("DISCORD_TOKEN")
+    bot.run(discord_key)
+    #print(message.text)
+    #print(f"Prompt tokens: {message.usage_metadata.prompt_token_count}")
+    
 if __name__ == "__main__":
     main()
