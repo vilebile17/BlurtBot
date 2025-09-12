9c94dda502cf438fd8a43d72641b4c45dc4211dc
vilebile_17 <ibrahimsomauroo@proton.me>
2025-09-12 16:05:26 +0100

    did a bit of tidying

--- gemini.py
+++ gemini.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-def get_message(previous_messages):
+def predict_message(previous_messages):
     # Uses gemini to predict the next message based on a list of previous messages
 
     load_dotenv()
@@ -15,6 +15,7 @@ def get_message(previous_messages):
     You will recieve a list of past messages (the first item is the most recent and the last item is the oldest message in the sample) and with that you must align to the tone, abreviations (or lack of them), message length and context to try and be as human as possible.
     I hope you are up for the challenge.
     All that I need you to return is the message you will send; ** NO NAME OF SENDER **, no date, just simply the message.
+    You may use markdown formating, however based on the conversation tone that may not be appropriate
     """
 
     response = client.models.generate_content(
--- main.py
+++ main.py
@@ -2,30 +2,10 @@ import os, sys, discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
-from gemini import get_message
+from gemini import predict_message
 
 
 def main():
-    message = get_message([
-"""Yakha — 23/07/2025, 03:03
-I shall ask my birthers if I may participate, if you would allow my presence that is""",
-'''XSET-IGL
-[GOAT]
- — 23/07/2025, 03:03
-🤓''',
-"""peasandbeans34
-[FC]
- — 23/07/2025, 10:00
-I shall also ask my birther if I may participate, if you would allow my presence that is 
-""",
-"""Ibrahim — 23/07/2025, 11:29
-of course you can all come but i need to 
-remind you i've got 6 seats in my car""",
-"""peasandbeans34
-[FC]
- — 23/07/2025, 13:10
-we can probably get *escorted* by our birther"""
-    ])
 
     intents = discord.Intents.default()
     intents.message_content = True
@@ -60,24 +40,22 @@ we can probably get *escorted* by our birther"""
         if "hi" in message.content.lower() and "blurt" in message.content.lower():
             await message.channel.send(f"Hello there, {message.author}")
         
-        elif message.content.lower() == "predict":
+        elif message.content.lower() == "!predict":
             history = message.channel.history(limit=50)
             msg_lst = []
             async for msg in history:
-                if msg.content != "predict" and msg.author != client.user:
+                if not msg.content.startswith("!") and msg.author != client.user:
                     msg_lst.append(msg.content)
 
             print(msg_lst)
             if history:
-                prediction = get_message(msg_lst)
+                prediction = predict_message(msg_lst)
                 await message.channel.send(prediction)
             else:
                 print("Either message history is empty or the permission to view history is off")
 
-    client.run(discord_key)
 
-    #print(message.text)
-    #print(f"Prompt tokens: {message.usage_metadata.prompt_token_count}")
+    client.run(discord_key)
     
 if __name__ == "__main__":
     main()
